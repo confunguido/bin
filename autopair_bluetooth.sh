d10b50aee7dfe164eb91db517f4bf0ce4d24a5cd
confunguido <confunguido@gmail.com>
2018-09-12 22:29:51 -0400

    1 for trekz 2 for gonovate

--- autopair_bluetooth.sh
+++ autopair_bluetooth.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-bluetoothctl << EOF
-connect 20:74:CF:0B:62:F8
-EOF
+if [$1 -eq 1]
+then
+    bluetoothctl << EOF
+    connect 20:74:CF:0B:62:F8
+    EOF
+else if [ $1 -eq 2 ]
+then
+    bluetoothctl <<EOF
+    connect 00:11:67:11:1C:5C
+    EOF
+fi
 
