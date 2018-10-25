5ec60c4a8aebf2000be1e8d64344a4c0d78fdc5f
confunguido <confunguido@gmail.com>
2018-10-25 19:48:47 -0400

    updated scripts for bluetooth

--- autopair_bluetooth.sh
+++ autopair_bluetooth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [$1 -eq 1]
 then
-    bluetoothctl << EOF
+    bluetoothctl<<EOF
     connect 20:74:CF:0B:62:F8
-    EOF
+EOF
 else if [ $1 -eq 2 ]
 then
-    bluetoothctl <<EOF
+    bluetoothctl<<EOF
     connect 00:11:67:11:1C:5C
-    EOF
+EOF
 fi
 
