bd6de6340030d4ef62d6ac6d17d889224f4f282e
Guido Espana <confunguido@gmail.com>
2021-09-13 10:08:56 -0400

    Automatically changing webcam settings

--- set_webcam.sh
+++ set_webcam.sh
@@ -4,3 +4,9 @@ v4l2-ctl -d /dev/video0 -c zoom_absolute=3
 v4l2-ctl -d /dev/video0 -c pan_absolute=32500
 v4l2-ctl -d /dev/video0 -c tilt_absolute=15000
 v4l2-ctl -d /dev/video0 --list-ctrls
+
+v4l2-ctl -d /dev/video2 -c brightness=-15
+v4l2-ctl -d /dev/video2 -c zoom_absolute=3
+v4l2-ctl -d /dev/video2 -c pan_absolute=32500
+v4l2-ctl -d /dev/video2 -c tilt_absolute=15000
+v4l2-ctl -d /dev/video2 --list-ctrls
